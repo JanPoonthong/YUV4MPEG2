1fc37518a0f5eb648d5168dddd771d4ab6a2e838
Jan Poonthong <janpoonthong628@gmail.com>
2021-08-28 20:25:06 +0700

    Remove unused functions

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
 #define WIDTH 800
 #define HEIGHT 600
@@ -8,75 +8,17 @@
 #define FRAMES_COUNT (FPS * DURATION)
 #define PIXELS_SIZE (WIDTH * HEIGHT)
 
-typedef struct
-{
+typedef struct {
   int y, cb, cr;
 } YCbCr;
 
-void save_ppm_file(int pixels[], int stride)
-{
-  int w = stride;
-  int h = PIXELS_SIZE / stride;
-  FILE *out_put = fopen("output.ppm", "w");
-  const int buffer_size = 3;
-
-  fprintf(out_put, "P6\n%i %i 255\n", w, h);
-  for (int i = 0; i < PIXELS_SIZE; i++)
-  {
-    char buffer[] = {
-        (pixels[i] >> 8 * 2) & 0xFF,
-        (pixels[i] >> 8 * 1) & 0xFF,
-        (pixels[i] >> 8 * 0) & 0xFF,
-    };
-    fwrite(buffer, 1, buffer_size, out_put);
-  }
-
-  fclose(out_put);
-}
-
-void uv_gradient_pattern(int stride, int pixels[])
-{
-  int width = stride;
-  int height = PIXELS_SIZE / stride;
-  for (int y = 0; y < height; y++)
-  {
-    for (int x = 0; x < width; x++)
-    {
-      float u = (float)x / width;
-      float v = (float)y / height;
-      int r = u * 255;
-      int g = v * 255;
-      pixels[y * width + x] = (r << 8 * 2) | (g << 8 * 1);
-    }
-  }
-}
-
-void assign_zero_color(int pixels[])
-{
-  for (int i = 0; i < PIXELS_SIZE; i++)
-  {
+void assign_zero_color(int pixels[]) {
+  for (int i = 0; i < PIXELS_SIZE; i++) {
     pixels[i] = 0;
   }
 }
 
-void random_pattern(int pixels[], int stride, int luma)
-{
-  int w = stride;
-  int h = PIXELS_SIZE / w;
-  for (int y = 0; y < h; y++)
-  {
-    for (int x = 0; x < w; x++)
-    {
-      int cb = x & 0xFF;
-      int cr = y & 0xFF;
-      pixels[y * stride + x] =
-          (luma << (8 * 2)) | (cb << (8 * 0)) | (cr << (8 * 1));
-    }
-  }
-}
-
-YCbCr rgb_conveter(int r, int g, int b)
-{
+YCbCr rgb_conveter(int r, int g, int b) {
   return (YCbCr){
       .y = 16 + (65.738 * r + 129.057 * g + 25.064 * b) / 256,
       .cb = 128 + (-37.945 * r - 74.494 * g + 112.439 * b) / 256,
@@ -84,8 +26,7 @@ YCbCr rgb_conveter(int r, int g, int b)
   };
 }
 
-int main(void)
-{
+int main(void) {
   int pixels[WIDTH * HEIGHT];
   assign_zero_color(pixels);
 
@@ -94,21 +35,17 @@ int main(void)
           FRAMES_COUNT);
 
   YCbCr ycbcr = rgb_conveter(138, 43, 226);
-  for (int frame = 0; frame < FRAMES_COUNT; frame++)
-  {
+  for (int frame = 0; frame < FRAMES_COUNT; frame++) {
     fprintf(out_put, "FRAME\n");
-    for (int j = 0; j < PIXELS_SIZE; j++)
-    {
+    for (int j = 0; j < PIXELS_SIZE; j++) {
       char pixels[] = {ycbcr.y};
       fwrite(pixels, 1, 1, out_put);
     }
-    for (int j = 0; j < PIXELS_SIZE; j++)
-    {
+    for (int j = 0; j < PIXELS_SIZE; j++) {
       char pixels[] = {ycbcr.cb};
       fwrite(pixels, 1, 1, out_put);
     }
-    for (int j = 0; j < PIXELS_SIZE; j++)
-    {
+    for (int j = 0; j < PIXELS_SIZE; j++) {
       char pixels[] = {ycbcr.cr};
       fwrite(pixels, 1, 1, out_put);
     }
